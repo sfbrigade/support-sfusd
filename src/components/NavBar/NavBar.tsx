96d97b27a131c9eef5cd6ede01c094cdf9361769
truham <hamiltontruong@gmail.com>
2023-09-29 22:02:55 -0700

    Navbar updates

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <div className="fixed w-full bg-gray-800 text-white p-4">
+    <div className="fixed w-full bg-white text-black p-4">
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
           {/* Home Icon */}
@@ -24,47 +24,35 @@ const Navbar = () => {
             </span>
           </Link>
 
+          {/* About Link on desktop */}
+          <div className="hidden md:block">
+            <Link href="/about">
+              About
+            </Link>
+          </div>
+
           {/* Hamburger Icon */}
-          <Hamburger toggled={isOpen} toggle={setOpen} />
+          <div className="md:hidden">
+            <Hamburger toggled={isOpen} toggle={setOpen} color="black" />
+          </div>
         </div>
 
-        {/* Navigation Tabs */}
+        {/* Mobile Menu */}
         <div
-          className={`fixed top-16 left-0 w-full flex flex-col items-center justify-start bg-gray-800 text-white bg-opacity-90 h-screen pt-4 transition-opacity duration-1000 ${
+          className={`fixed top-16 left-0 w-full flex flex-col items-center justify-start bg-white text-black h-screen pt-4 transition-opacity duration-1000 ${
             isOpen
               ? "opacity-100 pointer-events-auto"
               : "opacity-0 pointer-events-none"
           }`}
         >
-          <Link
-            href="/nav1"
+          <Link href="/about"
             className={`py-4 transform transition-all duration-1000 ${
               isOpen
                 ? "translate-y-0 opacity-100 delay-200"
                 : "translate-y-[-10px] opacity-0"
             }`}
           >
-            Nav 1
-          </Link>
-          <Link
-            href="/nav2"
-            className={`py-4 transform transition-all duration-1000 ${
-              isOpen
-                ? "translate-y-0 opacity-100 delay-400"
-                : "translate-y-[-10px] opacity-0"
-            }`}
-          >
-            Nav 2
-          </Link>
-          <Link
-            href="/nav3"
-            className={`py-4 transform transition-all duration-1000 ${
-              isOpen
-                ? "translate-y-0 opacity-100 delay-600"
-                : "translate-y-[-10px] opacity-0"
-            }`}
-          >
-            Nav 3
+            About
           </Link>
 
           {/* Social Icons */}
