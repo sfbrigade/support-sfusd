75bfa6fc1f38618640265970b24e01eb038770b1
Brandon Cruz-Youll <42663258+BeeSeeWhy@users.noreply.github.com>
2023-10-02 13:38:11 -0700

    made some minor changes so icons will show

--- src/components/MapCard/MapCard.tsx
+++ src/components/MapCard/MapCard.tsx
@@ -33,7 +33,7 @@ const MapCard = (props: {
         </div>
         <div className="flex items-center mt-2">
           <img
-            src="/icons/ell-icon.png"
+            src="/icons/language-icon.png"
             alt="ELL Icon"
             className="w-5 h-5 mr-2"
           />
