21cf716c01c5445223c81d3dfabf920ac9dc8bd6
Armin Mahini <19381138+amahini95@users.noreply.github.com>
2023-10-06 16:42:10 -0700

    Boiler plate code for the about page. Need to flesh it out more + link the about page to our main webpage

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,17 @@
+// pages/index.tsx
+
+import React from 'react';
+// import React, { useState } from "react";
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Problem: people want to give back to schools but don’t have easy access to information on how to do it.</p>
+            <p>Solution: a user-friendly website that directs people on how to spend their time or money.</p>
+            <p>Goal: Increased philanthropy, increased volunteers, and increased attendance at SFUSD school events open to the public</p>
+        </div>
+    );
+};
+
+export default AboutPage;
