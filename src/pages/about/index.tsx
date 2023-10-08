3ff4110bd3a21b549d7c0479c51d076adc80c30a
Armin Mahini <19381138+amahini95@users.noreply.github.com>
2023-10-07 18:47:42 -0700

    Just getting started on 'About' page, need to clarify how to style/format it

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -2,11 +2,12 @@
 
 import React from 'react';
 // import React, { useState } from "react";
+import styles from './about.module.css';
 
-const AboutPage = () => {
+const AboutPage: React.FC = () => {
     return (
-        <div>
-            <h1>About Us</h1>
+        <div className={styles.header}>
+            <h1>About Us 🏫 </h1>
             <p>Problem: people want to give back to schools but don’t have easy access to information on how to do it.</p>
             <p>Solution: a user-friendly website that directs people on how to spend their time or money.</p>
             <p>Goal: Increased philanthropy, increased volunteers, and increased attendance at SFUSD school events open to the public</p>
