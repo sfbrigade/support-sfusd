5e0d72096e49e0ac2131b1fd1548487d05cbfc15
Megan <mvyenielo58@gmail.com>
2023-12-16 15:52:04 -0800

    adjusted size and spacing of icons

--- src/components/SchoolCardMap.tsx
+++ src/components/SchoolCardMap.tsx
@@ -26,28 +26,28 @@ const SchoolCard: React.FC<SchoolCardProps> = ({ school }) => {
       />
       <h3 className="text-xl font-bold mb-2">{school.name}</h3>
       <h3 className="text-base mb-2 text-slate-400">{school.district} District</h3>
-      <div>
-        <div className="flex flex-row items-center">
+      <div className="flex flex-col items-left">
+        <div className="flex flex-row items-center mb-2">
           <img
             src="icons/student-icon.png"
             alt="student icon"
-            className="max-w-[40px] max-h-[40px] mr-2"
+            className="max-w-[35px] max-h-[35px] mr-2"
           />
           <h3 className="text-base">{school.students  ? school.students : "N/A"} Students</h3>
         </div>
-        <div className="flex flex-row items-center">
+        <div className="flex flex-row items-center mb-2">
           <img
             src="icons/lunch-icon.png"
             alt="lunch icon"
-            className="max-w-[40px] max-h-[40px] mr-2"
+            className="max-w-[30px] max-h-[30px] mr-2"
           />
           <h3 className="text-base">{school.frl  ? school.frl : "N/A"}% Free and Reduced Lunch</h3>
         </div>
-        <div className="flex flex-row items-center">
+        <div className="flex flex-row items-center mb-2">
           <img
             src="icons/language-icon.png"
             alt="language icon"
-            className="max-w-[40px] max-h-[40px] mr-2"
+            className="max-w-[30px] max-h-[30px] mr-2"
           />
           <h3 className="text-base">{school.ell  ? school.ell : "N/A"}% English Language Learners</h3>
         </div>
