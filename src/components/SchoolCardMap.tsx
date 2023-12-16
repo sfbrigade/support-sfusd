5bd9b7fb6d14c28206f904446167a98210b83887
Megan <mvyenielo58@gmail.com>
2023-12-16 15:43:08 -0800

    adjusted img and font size to fit on card

--- src/components/SchoolCardMap.tsx
+++ src/components/SchoolCardMap.tsx
@@ -22,35 +22,37 @@ const SchoolCard: React.FC<SchoolCardProps> = ({ school }) => {
       <img
         src={school.img}
         alt={school.name}
-        className="w-full h-64 object-cover rounded-lg mb-4"
+        className="w-full h-40 object-cover rounded-lg mb-4"
       />
-      <h3 className="text-2xl font-bold mb-2">{school.name}</h3>
-      <h3 className="text-lg mb-2 text-slate-400">{school.district} District</h3>
-      <div className="flex flex-row items-center">
-        <img
-          src="icons/student-icon.png"
-          alt="student icon"
-          className="max-w-[40px] max-h-[40px] mr-2"
-        />
-        <h3 className="text-lg">{school.students  ? school.students : "N/A"} Students</h3>
+      <h3 className="text-xl font-bold mb-2">{school.name}</h3>
+      <h3 className="text-base mb-2 text-slate-400">{school.district} District</h3>
+      <div>
+        <div className="flex flex-row items-center">
+          <img
+            src="icons/student-icon.png"
+            alt="student icon"
+            className="max-w-[40px] max-h-[40px] mr-2"
+          />
+          <h3 className="text-base">{school.students  ? school.students : "N/A"} Students</h3>
+        </div>
+        <div className="flex flex-row items-center">
+          <img
+            src="icons/lunch-icon.png"
+            alt="lunch icon"
+            className="max-w-[40px] max-h-[40px] mr-2"
+          />
+          <h3 className="text-base">{school.frl  ? school.frl : "N/A"}% Free and Reduced Lunch</h3>
+        </div>
+        <div className="flex flex-row items-center">
+          <img
+            src="icons/language-icon.png"
+            alt="language icon"
+            className="max-w-[40px] max-h-[40px] mr-2"
+          />
+          <h3 className="text-base">{school.ell  ? school.ell : "N/A"}% English Language Learners</h3>
+        </div>
+        <p>{school.description}</p>
       </div>
-      <div className="flex flex-row items-center">
-        <img
-          src="icons/lunch-icon.png"
-          alt="lunch icon"
-          className="max-w-[40px] max-h-[40px] mr-2"
-        />
-        <h3 className="text-lg">{school.frl  ? school.frl : "N/A"}% Free and Reduced Lunch</h3>
-      </div>
-      <div className="flex flex-row items-center">
-        <img
-          src="icons/language-icon.png"
-          alt="language icon"
-          className="max-w-[40px] max-h-[40px] mr-2"
-        />
-        <h3 className="text-lg">{school.ell  ? school.ell : "N/A"}% English Language Learners</h3>
-      </div>
-      <p>{school.description}</p>
     </div>
   );
 };
