88fd2363d7919c53fc87d99857e4a32410370013
Megan <mvyenielo58@gmail.com>
2024-01-16 20:17:58 -0800

    started seed sql file for school data

---
+++ schools-seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO schools (name, address, district, students, frl, ell, color, img, lat, lng)
+VALUES ('Balboa High School',
+        '1000 Cayuga Ave, San Francisco, CA',
+        'Cayuga Terrace',
+        '1230',
+        '59',
+        '',
+        '#3986ff',
+        'img/balboa.jpg',
+        37.722,
+        -122.440413);
