95b5db65bf2d55fb5a7e3f8b0861154e129f7555
Karly Stark <karlystark@gmail.com>
2024-01-30 18:51:23 -0800

    added docstring and note for future MapList tickets

--- src/components/MapList.tsx
+++ src/components/MapList.tsx
@@ -2,7 +2,19 @@ import React from "react";
 import MapListCard from "./MapListCard";
 import schools from "../data/schools";
 
+/**
+ * MapList: Renders a collection of expandable school cards in the map's list view.
+ *
+ * Props: none
+ *
+ * State: none
+ *
+ * map => MapList => MapListCard
+ *
+ */
 const MapList = () => {
+  /* NOTE: Will need to create expandable card functionality in future version.
+  Currently the MapListCard is the "expanded" design. */
   return (
     <div className="block md:hidden flex flex-col p-4 mt-2">
       <div className="flex justify-start items-center mb-4">
