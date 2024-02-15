9fc4284761f07700b0b4b2f2f7f21f26ddd43559
poswalsameer <poswalsameer2003@gmail.com>
2024-02-15 18:38:34 +0530

    added the feature to scroll back to top of the screen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,9 +13,15 @@ const lato = Lato({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" }); 
+  };
+
   return (
     <Provider store={store}>
       <Layout>
+        <button className="fixed text-3xl z-10 bottom-10 right-10 h-14 w-14 bg-yellow-600 text-yellow-200 border-2 border-yellow-800 cursor-pointer rounded-xl" onClick={scrollToTop} >⇑</button>
         <div className={`${fredoka.variable} ${lato.variable}`}>
           <Component {...pageProps} />
         </div>
