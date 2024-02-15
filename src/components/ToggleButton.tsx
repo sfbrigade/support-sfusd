24fb186a7b7aa47823378ab5cc9958bcef2fafac
rooiss <louis.kim010@gmail.com>
2024-02-14 21:51:09 -0800

    toggle for desktop working

--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Image from 'next/image';
+import React from 'react'
+import Image from 'next/image'
 
 /** ToggleButton renders a toggle button with icon and text dependent on the view
  * state of our map component (Map or List views).
@@ -15,37 +15,39 @@ import Image from 'next/image';
  */
 
 type ToggleButtonProps = {
-    isMapView: boolean;
-    toggleView: () => void;
-};
+  isMapView: boolean
+  toggleView: () => void
+}
 
 const ToggleButton: React.FC<ToggleButtonProps> = ({
-    isMapView, toggleView
+  isMapView,
+  toggleView,
 }) => {
-    return (
-        <div className={`bg-stone-800 flex items-center space-x-2 fixed right-4 p-1
-        rounded-full md:hidden top-24 px-4 py-2 shadow-lg focus:outline-visible`}>
-            <button
-                className={`flex`}
-                onClick={toggleView}
-                type="button"
-                role="switch"
-                aria-checked={isMapView}
-                aria-label={isMapView ? "Switch to list view" : "Switch to map view"}
-            >
-                <span className={`text-gray-50 pr-2`}>
-                    {isMapView ? "Show List" : "Show Map"}
-                </span>
-                <Image
-                    src={isMapView ? "./icons/listview.svg" : "./icons/mapview.svg"}
-                    alt=""
-                    width={25}
-                    height={25}
-                />
-            </button>
-        </div>
-    );
-};
+  return (
+    <div
+      className={`bg-stone-800 flex items-center space-x-2 fixed right-4 p-1
+        rounded-full top-24 px-4 py-2 shadow-lg focus:outline-visible`}
+    >
+      <button
+        className={`flex`}
+        onClick={toggleView}
+        type="button"
+        role="switch"
+        aria-checked={isMapView}
+        aria-label={isMapView ? 'Switch to list view' : 'Switch to map view'}
+      >
+        <span className={`text-gray-50 pr-2`}>
+          {isMapView ? 'Show List' : 'Show Map'}
+        </span>
+        <Image
+          src={isMapView ? './icons/listview.svg' : './icons/mapview.svg'}
+          alt=""
+          width={25}
+          height={25}
+        />
+      </button>
+    </div>
+  )
+}
 
-
-export default ToggleButton;
+export default ToggleButton
--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import mapboxgl from 'mapbox-gl'
-import schools from '../data/schools'
 import SchoolCard from '../components/SchoolCardMap'
 import MapList from '@/components/MapList'
 import MapboxMap from '@/components/MapboxMap'
@@ -24,33 +22,35 @@ const Map = () => {
     setIsList(!isList)
   }
   return (
-    <div className="flex flex-col md:flex-row relative w-full h-[calc(100vh-80px)]">
-      <div>
+    <div className="flex flex-col relative w-full h-[calc(100vh-80px)]">
+      <div className="flex mt-16 justify-center">
         <ToggleButton isMapView={!isList} toggleView={handleToggle} />
       </div>
-      <div className="w-full h-1/6 md:w-1/2 md:h-full flex justify-center items-center">
-        {selectedSchool && (
-          <div className="hidden md:block">
-            {' '}
-            {/* Hide SchoolCard on screens smaller than md */}
-            <SchoolCard school={selectedSchool} />
-          </div>
-        )}
-        {!selectedSchool && (
-          <div className="flex flex-col justify-center items-center h-full">
-            <h1 className="text-4xl font-bold mb-4">Select a School</h1>
-            <p className="text-lg mb-4">
-              Click on a {isList ? 'school' : 'marker '} to view more
-              information.
-            </p>
-          </div>
+      <div className="flex flex-col md:flex-row h-full">
+        <div className="w-full h-1/6 md:w-1/2 md:h-full flex justify-center items-center">
+          {selectedSchool && (
+            <div className="hidden md:block">
+              {' '}
+              {/* Hide SchoolCard on screens smaller than md */}
+              <SchoolCard school={selectedSchool} />
+            </div>
+          )}
+          {!selectedSchool && (
+            <div className="flex flex-col justify-center items-center">
+              <h1 className="text-4xl font-bold mb-4">Select a School</h1>
+              <p className="text-lg mb-4">
+                Click on a {isList ? 'school' : 'marker '} to view more
+                information.
+              </p>
+            </div>
+          )}
+        </div>
+        {isList ? (
+          <MapList setSelectedSchool={setSelectedSchool} />
+        ) : (
+          <MapboxMap setSelectedSchool={setSelectedSchool} />
         )}
       </div>
-      {isList ? (
-        <MapList setSelectedSchool={setSelectedSchool} />
-      ) : (
-        <MapboxMap setSelectedSchool={setSelectedSchool} />
-      )}
     </div>
   )
 }
