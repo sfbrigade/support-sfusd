3dd07c12f76a120242b03d77c0dfe131106bf0a2
“Iryna <maria14trush@gmail.com>
2024-02-21 18:54:17 -0800

    modify onChange function

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,14 +2,19 @@ import React, { useState } from "react";
 
 export default function SearchBar() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [dropdownItems, setDropdownItems] = useState([]);
 
+  const onInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const userInput = e.target.value;
+    setSearchTerm(userInput);
+  };
   return (
     <div>
       <input
         type="text"
         placeholder="Type your school here..."
         value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+        onChange={onInputChange}
       />
     </div>
   );
