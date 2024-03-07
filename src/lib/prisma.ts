de10b8b4f286d9bd3f629ca458485a82ce01a12e
rooiss <louis.kim010@gmail.com>
2024-03-06 17:43:11 -0800

    changed prisma integration file to recognize type globalThis

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,13 +2,16 @@ import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
   }
-  prisma = global.prisma;
+  prisma = globalWithPrisma.prisma;
 }
 
 export default prisma;
