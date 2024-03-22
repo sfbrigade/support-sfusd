533dbff8227b12ef658c96a5bed0c1b4061f19f2
Brandon Cruz-Youll <42663258+BeeSeeWhy@users.noreply.github.com>
2024-03-22 16:42:52 -0700

    dropdown updates

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,26 +1,23 @@
 import React, { useState } from "react";
-import Schools from "@/data/schools";
-import Select from "react-select"
+import Dropdown from "./Dropdown";
 
-interface DropdownItem<ItemType> {
+export interface DropdownItem<ItemType> {
   label: string;
   value: string;
   item: ItemType;
 }
-
 interface SearchBarProps<DropdownItemType> {
   onItemSelect: (item: DropdownItem<DropdownItemType>) => void;
   onSearch: (searchTerm: string) => Promise<DropdownItem<DropdownItemType>[]>;
 }
 
-export default function SearchBar<DropdownItemType = any>({
+export default function SearchBar <DropdownItemType = any>({
   onItemSelect,
   onSearch,
 }: SearchBarProps<DropdownItemType>): JSX.Element {
-  const [searchTerm, setSearchTerm] = useState(""); 
+  const [searchTerm, setSearchTerm] = useState("");
   const [dropdownItems, setDropdownItems] = useState<
     DropdownItem<DropdownItemType>[]
-    
   >([]);
 
   const onInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -29,25 +26,15 @@ export default function SearchBar<DropdownItemType = any>({
     const searchResults = await onSearch(userInput);
     setDropdownItems(searchResults);
   };
-
-  const options = Schools.map((school, index) => {
-    return {
-       label: school.name,
-       value: school,
-       key: index
-    }
-});
-
   return (
     <div>
-      <Select
-        className="basic-single"
-        classNamePrefix="select"
-        defaultValue="Type your school here..."
-        onChange={onInputChange}
+      <input
+        type="text"
+        placeholder="Type your school here..."
         value={searchTerm}
-        options={dropdownItems}
+        onChange={onInputChange}
       />
+      {dropdownItems.length > 0 && searchTerm.length > 0 && (<Dropdown items={dropdownItems} onItemSelect={onItemSelect}/>)};
     </div>
-  );
-}
+   
+)}
