f765ca3a00906f9bbeb7b45d7fe8cc0b0f69f3e1
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2024-05-02 16:37:31 -0700

    constrain banner showing just to "/profile" page

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -3,13 +3,20 @@ import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/NavBar";
 import Banner from "@/components/Banner";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function RootLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const { pathname } = router;
+
   return (
     <div className={`${inter.className} px-0 md:px-4`}>
-      <Banner><strong>BETA:</strong> This website is in beta - let us know if you have any <a href="#" className="underline">feedback/questions</a><span className="hidden md:inline"> to help us improve it</span>.</Banner>
+      {
+        pathname.includes('/profile') &&
+          <Banner><strong>BETA:</strong> This website is in beta - let us know if you have any <a href="#" className="underline">feedback/questions</a><span className="hidden md:inline"> to help us improve it</span>.</Banner>
+      }
       <Navbar />
       <div className="">{children}</div>
       <Footer />
