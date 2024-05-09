1b200f945a21d7802f7c4f8230f468c415026474
Megan <mvyenielo58@gmail.com>
2024-05-08 17:19:45 -0700

    connected Contact form to EmailJS

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import  {useRef} from 'react';
+import  React from 'react';
+import emailjs from '@emailjs/browser';
 
 
 function ContactUs() {
+
+  //const form = useRef();
+  //const form = React.useRef<HTMLFormElement>()
+  const form = React.useRef<HTMLFormElement | null>(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (form.current) {
+      emailjs
+        .sendForm('service_itlkzak', 'template_ee6s74u', form.current, {
+          publicKey: '10-NnnxJFw9zLmYPf',
+        })
+        .then(
+          () => {
+            console.log('SUCCESS!');
+          },
+          (error) => {
+            console.log('FAILED...', error.text);
+          },
+        );
+    }
+  };
+
   return (
     <div>
-      <form className="max-w-md mx-auto mt-5 mb-3 p-6">
+      <form ref={form} onSubmit={sendEmail} className="max-w-md mx-auto mt-5 mb-3 p-6">
         <div className="mb-4">
           <label className="block text-gray-500 mb-2" htmlFor="name">
             Name:
@@ -15,6 +41,7 @@ function ContactUs() {
                   id="name"
                   type="text"
                   placeholder="Enter your name"
+                  name="user_name"
           />
         </div>
         <div className="mb-4">
@@ -27,6 +54,7 @@ function ContactUs() {
                   id="email"
                   type="email"
                   placeholder="Enter your email"
+                  name="user_email"
           />
         </div>
         <div className="mb-4">
@@ -38,7 +66,10 @@ function ContactUs() {
                              focus:outline-none focus:shadow-outline"
                     id="message"
                     rows={5}
-                    placeholder="Write your message here"></textarea>
+                    placeholder="Write your message here"
+                    name="message"
+                    >
+          </textarea>
         </div>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold
                          py-2 px-4 rounded focus:outline-none focus:shadow-outline"
