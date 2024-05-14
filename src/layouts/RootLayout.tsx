fa5009b52717b147d7bafef4ba6aaf866f679423
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2024-05-14 14:03:59 -0700

    move beta banner back to profile page

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -15,7 +15,7 @@ function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <div id="root" className={`${inter.className} px-0 md:px-4 flex flex-col ${pathname.includes('/map') ? 'h-screen' : 'h-auto'}`}>
       {
-        pathname.includes('/map') &&
+        pathname.includes('/profile') &&
           <Banner><strong>BETA:</strong> This website is in beta - let us know if you have any <Link href="/about" className="underline">feedback/questions</Link><span className="hidden md:inline"> to help us improve it</span>.</Banner>
       }
       <Navbar />
