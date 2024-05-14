cfc178ff572c16a11e80a028fa93edc9314cc517
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2024-05-14 14:01:59 -0700

    hook up beta banner link w/about page

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/NavBar";
 import Banner from "@/components/Banner";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +15,8 @@ function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <div id="root" className={`${inter.className} px-0 md:px-4 flex flex-col ${pathname.includes('/map') ? 'h-screen' : 'h-auto'}`}>
       {
-        pathname.includes('/profile') &&
-          <Banner><strong>BETA:</strong> This website is in beta - let us know if you have any <a href="#" className="underline">feedback/questions</a><span className="hidden md:inline"> to help us improve it</span>.</Banner>
+        pathname.includes('/map') &&
+          <Banner><strong>BETA:</strong> This website is in beta - let us know if you have any <Link href="/about" className="underline">feedback/questions</Link><span className="hidden md:inline"> to help us improve it</span>.</Banner>
       }
       <Navbar />
       <div className="flex-1">{children}</div>
