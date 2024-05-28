ca4bacfa6b7762f7174ee5a33a6ebf64483ede38
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2024-05-28 10:10:37 -0700

    slightly refactor vh fallback code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
 import plugin from "tailwindcss/plugin";
 
+const vh_fallback = "100vh /* fallback for Opera, IE and etc. */";
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -23,24 +25,25 @@ const config: Config = {
   },
   plugins: [
     plugin(function ({ addBase, addComponents, addUtilities, theme }) {
+      /* NOTE: we are using the below instead of `theme.extend` to avoid the build errors while of arrays for values */
       addUtilities({
         ".h-dvh-with-fallback": {
-          height: ["100vh /* fallback for Opera, IE and etc. */", "100dvh"],
+          height: [vh_fallback, "100dvh"],
         },
         ".h-svh-with-fallback": {
-          height: ["100vh /* fallback for Opera, IE and etc. */", "100svh"],
+          height: [vh_fallback, "100svh"],
         },
         ".h-lvh-with-fallback": {
-          height: ["100vh /* fallback for Opera, IE and etc. */", "100lvh"],
+          height: [vh_fallback, "100lvh"],
         },
         ".h-min-dvh-with-fallback": {
-          minHeight: ["100vh /* fallback for Opera, IE and etc. */", "100dvh"],
+          minHeight: [vh_fallback, "100dvh"],
         },
         ".h-min-svh-with-fallback": {
-          minHeight: ["100vh /* fallback for Opera, IE and etc. */", "100svh"],
+          minHeight: [vh_fallback, "100svh"],
         },
         ".h-min-lvh-with-fallback": {
-          minHeight: ["100vh /* fallback for Opera, IE and etc. */", "100lvh"],
+          minHeight: [vh_fallback, "100lvh"],
         },
       });
     }),
