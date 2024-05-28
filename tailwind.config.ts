0ea1c5a0d40a5fc7d41f83d220db6de61c986750
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2024-05-28 01:55:24 -0700

    replace 100vh global CSS fix with tailwind config fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,25 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      /*
+      fix for browsers like mobile Safari not
+      playing well with `vh`
+      (see: https://stackoverflow.com/questions/75079019/tailwind-css-fallback-for-new-screen-length-types-such-as-lvh-svh, https://dev.to/frehner/css-vh-dvh-lvh-svh-and-vw-units-27k4, https://github.com/tailwindlabs/tailwindcss/discussions/8216, https://github.com/tailwindlabs/tailwindcss/pull/11317)
+      */
+      height: {
+        "dvh-with-fallback": [
+          "100vh /* fallback for Opera, IE and etc. */",
+          "100dvh",
+        ],
+        "svh-with-fallback": [
+          "100vh /* fallback for Opera, IE and etc. */",
+          "100svh",
+        ],
+        "lvh-with-fallback": [
+          "100vh /* fallback for Opera, IE and etc. */",
+          "100lvh",
+        ],
+      },
     },
   },
   plugins: [],
