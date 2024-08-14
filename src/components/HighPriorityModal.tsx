81578a5a7800a87733d185e46a235029115ba8ef
Brandon Cruz-Youll <42663258+BeeSeeWhy@users.noreply.github.com>
2024-08-14 14:32:10 -0700

    tried to correct warning for modal

--- src/components/HighPriorityModal.tsx
+++ src/components/HighPriorityModal.tsx
@@ -14,7 +14,7 @@ const customStyles = {
 };
 
 const HighPriorityModal: React.FC<ModalProps> = ({ onClose, isOpen }) => {
-  //Modal.setAppElement("body");
+  Modal.setAppElement("body");
   return (
     <Modal
       isOpen={isOpen}
