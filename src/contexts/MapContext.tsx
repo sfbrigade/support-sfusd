389f541a08bde9803a1e72e4d60e578dbc96b9be
thom <thomhickey@gmail.com>
2024-09-27 14:01:33 -0700

    fix: cq

--- src/contexts/MapContext.tsx
+++ src/contexts/MapContext.tsx
@@ -1,6 +1,20 @@
 import React, { createContext, useState, useContext } from 'react';
 import { School } from '@/types/school';
 
+/**
+ * MapContext: Manages state for the map view and school card so that if the user
+ * clicks on a school card, then clicks the back button to return to the map view,
+ * they are returned to the same place on the map that they were at when they
+ * clicked on the school card originally whether in map or list view and with
+ * correct school selected.
+ *
+ * Props:
+ *  - isMapView: boolean indicating whether the map view is currently active
+ *  - selectedSchool: School object representing the currently selected school
+ *  - setIsMapView: function to set the isMapView state (toggle between map and list view)
+ *  - setSelectedSchool: function to set the selectedSchool state (show/hide school card)     
+ */ 
+
 interface MapContextType {
   isMapView: boolean;
   selectedSchool: School | null;
