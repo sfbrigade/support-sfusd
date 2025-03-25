81d19cc9cd5589ae92494208e373e1415f335b0f
Matthew Gianni <matt@gianni.org>
2025-03-24 17:25:10 -0700

    added a temporary image review panel at /review to confirm stock imgs

---
+++ src/pages/review.tsx
@@ -0,0 +1,42 @@
+import Card from "@/components/schoolPageComponents/Card";
+import Link from "next/link";
+
+const review = () => {
+  return (
+    <div className="relative mx-auto flex flex-col gap-10 p-6 pt-2 md:py-20 lg:w-4/5 2xl:w-2/3">
+      <div className="text-4xl text-blue-500">Stock Image Review</div>
+      <div className="flex flex-col gap-10">
+        <div className="relative grid gap-8 sm:grid-cols-3">
+          <Link href="/review/K5">
+            <Card
+              title="K-5"
+              description="Elementary School Images"
+              img="http://localhost:3000/stock-images/K5/event/pexels-artempodrez-8088096.jpg"
+              index={1}
+            />
+          </Link>
+
+          <Link href="/review/MS">
+            <Card
+              title="Middle School"
+              description="Middle School Images"
+              img="/stock-images/MS/event/pexels-rdne-8500302.jpg"
+              index={2}
+            />
+          </Link>
+
+          <Link href="/review/HS">
+            <Card
+              title="High School"
+              description="High School Images"
+              img="/stock-images/HS/tutoring/pexels-mart-production-8472987.jpg"
+              index={3}
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default review;
