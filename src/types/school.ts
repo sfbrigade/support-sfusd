4ca19b95db67864da7ce07323b7d33097369afff
Matthew Gianni <matt@gianni.org>
2025-04-30 10:33:03 -0700

    removed commented old code

--- src/types/school.ts
+++ src/types/school.ts
@@ -25,23 +25,6 @@ export interface School {
   zipcode?: string;
 }
 
-// export interface SchoolProfile {
-//   about: string;
-//   about_bp: string[];
-//   volunteer_form_url: string;
-//   donation_url?: string;
-//   donation_text: string;
-//   testimonial?: string;
-//   testimonial_author?: string;
-//   testimonial_video?: string;
-//   testimonial_img?: string;
-//   noteable_video?: string;
-//   principal: string;
-//   instagram_url?: string;
-//   facebook_url?: string;
-//   website_url?: string;
-// }
-
 export interface Metric {
   name: string;
   value: number;
