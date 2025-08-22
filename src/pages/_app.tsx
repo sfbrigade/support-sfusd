89ec03f6300895353592b40f333ff84e21b0a4cd
Brandon Cruz-Youll <42663258+BeeSeeWhy@users.noreply.github.com>
2025-08-22 10:45:51 -0700

    adding posthog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,9 @@ import Layout from "../layouts/RootLayout";
 import { Fredoka, Lato } from "next/font/google";
 import Head from "next/head";
 import { MapProvider } from "../contexts/MapContext";
+import { useEffect } from "react";
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react'
 
 const fredoka = Fredoka({ subsets: ["latin"], variable: "--font-fredoka" });
 const lato = Lato({
@@ -13,6 +16,19 @@ const lato = Lato({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+      person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
+      defaults: '2025-05-24',
+      // Enable debug mode in development
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === 'development') posthog.debug()
+      }
+    })
+  }, [])
+  
   return (
     <>
       <Head>
@@ -21,7 +37,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       <MapProvider>
         <Layout>
           <div className={`${fredoka.variable} ${lato.variable} h-full`}>
-            <Component {...pageProps} />
+            <PostHogProvider client={ posthog } >
+              <Component {...pageProps} />
+            </PostHogProvider>
           </div>
         </Layout>
       </MapProvider>
