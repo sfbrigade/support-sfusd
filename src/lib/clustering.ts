ac0e78fe2f86eab4cd1de84d4784b1865f0e3bf9
alenavin <alenb.v@yandex.ru>
2026-02-05 22:10:08 -0800

    added marker clustering

---
+++ src/lib/clustering.ts
@@ -0,0 +1,54 @@
+import { School } from "@/types/school";
+import Supercluster from "supercluster";
+
+export interface SchoolProperties {
+  school: School;
+}
+
+export interface ClusterProperties {
+  cluster: true;
+  cluster_id: number;
+  point_count: number;
+  point_count_abbreviated: number | string;
+}
+
+export type SchoolPoint = GeoJSON.Feature<GeoJSON.Point, SchoolProperties>;
+export type ClusterPoint = GeoJSON.Feature<GeoJSON.Point, ClusterProperties>;
+export type MapPoint = SchoolPoint | ClusterPoint;
+
+export function schoolsToGeoJSON(
+  schools: School[]
+): GeoJSON.FeatureCollection<GeoJSON.Point, SchoolProperties> {
+  return {
+    type: "FeatureCollection",
+    features: schools
+      .filter((school) => school.latitude && school.longitude)
+      .map((school) => ({
+        type: "Feature",
+        properties: { school },
+        geometry: {
+          type: "Point",
+          coordinates: [Number(school.longitude), Number(school.latitude)],
+        },
+      })),
+  };
+}
+
+export function isCluster(
+  point: MapPoint
+): point is ClusterPoint {
+  return (point.properties as ClusterProperties).cluster === true;
+}
+
+export function createClusterIndex(schools: School[]): Supercluster<SchoolProperties, ClusterProperties> {
+  const index = new Supercluster<SchoolProperties, ClusterProperties>({
+    radius: 50,     // Cluster radius in pixels
+    maxZoom: 14,    // Stop clustering at this zoom level
+    minPoints: 2,   // Minimum points to form a cluster
+  });
+
+  const geoJson = schoolsToGeoJSON(schools);
+  index.load(geoJson.features);
+
+  return index;
+}
