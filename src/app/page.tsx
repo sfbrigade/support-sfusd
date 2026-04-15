75030a1c67d736ac04f92810211303db47c2245f
alenavin <alenb.v@yandex.ru>
2026-04-15 21:02:10 +0300

    added time to revalidate + volunteer list refactoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import HomeClient from "./HomeClient";
 
+export const revalidate = 3600; // cache page for 1 hour
+
 export const metadata: Metadata = {
   title: "Support SF Schools - Home",
 };
