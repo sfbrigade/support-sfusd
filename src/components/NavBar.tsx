01f319cc7762a4148837036255907184d8c27253
Qianying Chai <qianying.chai.dev@gmail.com>
2026-05-25 16:11:51 -0700

    feature : add How It Works link with placeholder page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -35,6 +35,7 @@ const Navbar = () => {
           {/* Links on desktop */}
           <div className="space-x-10 md:flex">
             <Link href="/map">Explore Schools</Link>
+            <Link href="/how-it-works">How It Works</Link>
             <Link href="/about">About Us</Link>
           </div>
         </div>
